388114505584bbd6a1ca2f2d90b1cfc4788754bd
Luis Ovando <isc.luisovando@gmail.com>
2018-11-09 14:35:59 -0600

    Add callback for execute before an object persist

--- src/main/java/com/luisovando/springboot/app/models/entity/Client.java
+++ src/main/java/com/luisovando/springboot/app/models/entity/Client.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -35,6 +36,11 @@ public class Client implements Serializable {
 	@Column(name="created_at")
 	@Temporal(TemporalType.DATE)
 	private Date createdAt;
+	
+	@PrePersist
+	public void beforePersist() {
+		this.createdAt = new Date();
+	}
 	
 	public Long getId() {
 		return id;
