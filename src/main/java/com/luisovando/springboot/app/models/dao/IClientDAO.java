5bf9bfa35201ed584e958ec2718d71645a2b05a2
Luis Ovando <isc.luisovando@gmail.com>
2018-11-09 12:32:59 -0600

    Create ClientDAO interface

---
+++ src/main/java/com/luisovando/springboot/app/models/dao/IClientDAO.java
@@ -0,0 +1,9 @@
+package com.luisovando.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.luisovando.springboot.app.models.entity.Client;
+
+public interface IClientDAO {
+	public List<Client> findAll();
+}
