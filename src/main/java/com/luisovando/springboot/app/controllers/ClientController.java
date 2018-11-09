517c398710274883caf54fc4b9843c6df22227cd
Luis Ovando <isc.luisovando@gmail.com>
2018-11-09 12:47:28 -0600

    Find all clients

---
+++ src/main/java/com/luisovando/springboot/app/controllers/ClientController.java
@@ -0,0 +1,23 @@
+package com.luisovando.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.luisovando.springboot.app.models.dao.IClientDAO;
+
+@Controller
+public class ClientController {
+	
+	@Autowired
+	private IClientDAO clientRepository;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/list")
+	public String getClients(Model model) {
+		model.addAttribute("title", "Client's List");
+		model.addAttribute("clients", clientRepository.findAll());
+		return "clientList";
+	}
+}
