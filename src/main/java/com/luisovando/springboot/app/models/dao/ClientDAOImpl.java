dd9cfd3fb2f403bb6278a08355eeaa50a80ee0d0
Luis Ovando <isc.luisovando@gmail.com>
2018-11-09 12:33:29 -0600

    Create ClientDAO implementation class

---
+++ src/main/java/com/luisovando/springboot/app/models/dao/ClientDAOImpl.java
@@ -0,0 +1,27 @@
+package com.luisovando.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luisovando.springboot.app.models.entity.Client;
+
+@Repository
+public class ClientDAOImpl implements IClientDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly=true)
+	public List<Client> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Client").getResultList();
+	}
+
+}
