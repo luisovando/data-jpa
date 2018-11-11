a0a90ed0c07ef8e3e3d5dcdf6904f31443ea5a07
Luis Ovando <isc.luisovando@gmail.com>
2018-11-11 15:01:17 -0600

    Add form edit behavior

--- src/main/java/com/luisovando/springboot/app/controllers/ClientController.java
+++ src/main/java/com/luisovando/springboot/app/controllers/ClientController.java
@@ -2,17 +2,24 @@ package com.luisovando.springboot.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.luisovando.springboot.app.models.dao.IClientDAO;
 import com.luisovando.springboot.app.models.entity.Client;
 
 @Controller
+@SessionAttributes("client")
 public class ClientController {
 	
 	@Autowired
@@ -34,9 +41,24 @@ public class ClientController {
 		return "form";
 	}
 	
+	@RequestMapping(value="/form/{id}")
+	public String edit(@PathVariable(value="id") Long clientId, Map<String, Object> model) {
+		if (clientId <= 0) {
+			return "redirect:list";
+		}
+		Client client = clientRepository.findOne(clientId);
+		model.put("title", "Editar de cliente");
+		model.put("client", client);
+		return "form";
+	}
+	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String store(Client client) {
+	public String store(@Valid Client client, BindingResult result, Map<String, Object> model, SessionStatus status) {
+		if (result.hasErrors()) {
+			return "form";
+		}
 		clientRepository.save(client);
+		status.setComplete();
 		return "redirect:list";
 	}
 }
